9f77c9327ff8e6549979055689b7d10d0aaf1056
Sergey Platonov <platonov.ser@gmail.com>
2017-07-04 22:54:21 +0300

    Workshop tickets added

--- partials/buytickets.php
+++ partials/buytickets.php
@@ -5,11 +5,8 @@
             <div class="buy-tickets-left row-col">
                 <a name="tickets" class="cpp-e_hash-link"></a>
                 <h2 class="cpp-e_block-title">
-                    Купить билет
+                    Купить билет на основную программу
                 </h2>
-                <div class="cpp-b_block-text">
-                    <p>Для тех, кто не может приехать<br>на конференцию, мы организовали<br>онлайн-трансляцию.  Присоединяйтесь!</p>
-                </div>
                 <div class="online">
                     <img src="/app/frontend/template/elem-buy-tickets.svg" alt="">
                 </div>
@@ -19,6 +16,40 @@
                     <a href="https://c-user-group-russia.timepad.ru/event/483848/" data-twf-placeholder="yes">Перейтик заказу билетов</a>
                     <script type="text/javascript" defer="defer" charset="UTF-8" data-timepad-customized="24933"
                         data-twf2s-event--id="483848" data-timepad-widget-v2="event_register"
+                        src="https://timepad.ru/js/tpwf/loader/min/loader.js">
+                        (function() {
+                            return {
+                                "loadCSS": [
+                                    "http://siberia-2017.cppconf.ru/app/build/css/timepad.css"
+                                ],
+                                "skipBaseCSS": true,
+                                "disableBootstrap": true
+                            };
+                        })();
+                    </script>
+                </div>
+                <div class="cpp-b_only-mobile">
+                    <a style="display: block;    margin: 0 auto;" class="cpp-e_green-button" href="https://c-user-group-russia.timepad.ru/event/483848/">Купить билет</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cpp-r_wrap">
+        <div class="cpp-b_buy-tickets cpp-b_responsive-row-cols">
+            <div class="buy-tickets-left row-col">
+                <a name="tickets" class="cpp-e_hash-link"></a>
+                <h2 class="cpp-e_block-title">
+                    Купить билет на масетр-класс
+                </h2>
+                <div class="online">
+                    <img src="/app/frontend/template/elem-buy-tickets.svg" alt="">
+                </div>
+            </div>
+            <div class="buy-tickets-right row-col">
+                <div class="cpp-b_only-desktop">
+                    <a href="https://c-user-group-russia.timepad.ru/event/511695/" data-twf-placeholder="yes">Перейтик заказу билетов</a>
+                    <script type="text/javascript" defer="defer" charset="UTF-8" data-timepad-customized="3453"
+                        data-twf2s-event--id="511695" data-timepad-widget-v2="event_register"
                         src="https://timepad.ru/js/tpwf/loader/min/loader.js">
                         (function() {
                             return {
