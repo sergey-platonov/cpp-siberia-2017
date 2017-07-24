c2865d9c187cad62e963257766477566b2e070c3
Sergey Platonov <sermp@yandex-team.ru>
2017-07-24 23:14:42 +0300

    Longup added

--- partials/sponsors.php
+++ partials/sponsors.php
@@ -6,11 +6,9 @@
             Gold спонсоры
         </h2>
         <div class="cpp-b_sponsors cpp-b_responsive-row-cols">
-            <!--
             <div class="sponsor row-col">
                 <a href="https://yandex.ru/company/"><img src="/app/frontend/template/partners/yandex_logo.png" alt=""></a>
             </div>
-             -->
             <div class="sponsor row-col">
                 <a href="https://infotecs.ru/"><img src="/app/frontend/template/partners/infotecs_logo.png" alt=""></a>
             </div>           
@@ -23,20 +21,22 @@
 </section>
 <!-- /Gold спонсоры -->
 <!-- Silver спонсоры -->
-<!--
+<!-- /Silver спонсоры -->
+
+<!-- Инфопартнеры  -->
 <section class="cpp-b_section">
     <div class="cpp-r_wrap">
+        <a name="sponsors" class="cpp-e_hash-link"></a>
         <h2 class="cpp-e_block-title">
-            Silver спонсоры
+           Информационные партнёры 
         </h2>
         <div class="cpp-b_sponsors cpp-b_responsive-row-cols">
-            <!-- 
             <div class="sponsor row-col">
-                <a href="http://www.viva64.com/en/pvs-studio/"><img src="/app/frontend/template/partners/unicorn.png" alt=""></a>
-            </div>
-            <!-- 
+                <a href="https://www.facebook.com/youlongup/"><img src="/app/frontend/template/partners/longup.jpg" alt=""></a>
+            </div>           
+ 	   <!-- -->
         </div>
     </div>
 </section>
--->
-<!-- /Silver спонсоры -->
+<!-- /Инфопартнеры -->
+
